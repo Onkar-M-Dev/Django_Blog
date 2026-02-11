ce0c5900a64e973dd7c6bd51a7427b6a32807015
Onkar-M-Dev <onkar.marnee1320@gmail.com>
2026-02-11 17:50:21 +0530

    "Updated"

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from blog.models import Post
 
 # Create your views here.
 def blogHome(request):
-    return render(request, 'blog/blogHome.html')
+    allPosts = Post.objects.all()
+    #print(allPosts)
+    context = {'allPosts':allPosts}
+    return render(request, 'blog/blogHome.html', context)
     #return HttpResponse("This is blogHome. We will keep all Blog posts here !")
 
 def blogPost(request, slug):
-    return render(request, 'blog/blogPost.html')
+    post = Post.objects.filter(slug=slug).first()
+    #print(post)
+    context = {'post':post}
+    return render(request, 'blog/blogPost.html', context)
     #return HttpResponse(f"This is blogPost: {slug}")
